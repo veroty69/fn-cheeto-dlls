5dc81d5c764ee0d671e3d7ec723c8cfcef793c6c
veroty69 <sleasy@rape.lol>
2020-12-08 16:53:50 +0100

    have fun hacking

---
+++ dlls/injector/Inject.bat
@@ -0,0 +1,30 @@
+@echo off
+
+color 4
+title Injector - by veroty#9999
+
+echo.
+echo [!] Press enter to inject.
+echo.
+
+pause
+
+altdrv2.exe
+cls
+modmap FortniteClient-Win64-Shipping.exe dxgi.dll cheat.dll
+
+timeout 2
+start "" https://discord.gg/pornhub
+color 2
+cls
+
+echo.
+echo [!] Successfully Injected!
+echo.
+
+timeout 3
+
+
+
+
+
